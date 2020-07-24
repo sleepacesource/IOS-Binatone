047452b6207c115e41aefcca3fe085e3dae14f5b
Shawsan@sleepace.net <Shawsan@sleepace.net>
2020-07-24 10:11:46 +0800

    update sdk, add crash log  and catch exception!

--- Binatone-demo/Binatone-demo/realtimeData/RealTimeDataViewController.m
+++ Binatone-demo/Binatone-demo/realtimeData/RealTimeDataViewController.m
@@ -163,19 +163,18 @@ enum {
     __weak typeof(self) weakSelf = self;
     [self showLoadingView];
     KFLog_Normal(YES, @"get 24 hour data");
-    //    [SLPBLESharedManager binatone:SharedDataManager.peripheral getLast24HourData:timestamp sex:1 callback:^(SLPDataTransferStatus status, id data) {
-    //        if (status == SLPDataTransferStatus_Succeed) {
-    //            [Coordinate pushToHistoryData:data type:E_HistoryDataType_24HourData sender:weakSelf animated:YES];
-    //        }else{
-    //            [Utils showDeviceOperationFailed:status atViewController:weakSelf];
-    //        }
-    //        [weakSelf unshowLoadingView];
-    //    }];
+//        [SLPBLESharedManager binatone:SharedDataManager.peripheral getLast24HourData:timestamp sex:1 callback:^(SLPDataTransferStatus status, id data) {
+//            if (status == SLPDataTransferStatus_Succeed) {
+//                [Coordinate pushToHistoryData:data type:E_HistoryDataType_24HourData sender:weakSelf animated:YES];
+//            }else{
+//                [Utils showDeviceOperationFailed:status atViewController:weakSelf];
+//            }
+//            [weakSelf unshowLoadingView];
+//        }];
     
     
     //analyse original data
     [SLPBLESharedManager binatone:SharedDataManager.peripheral getLast24HourOriginalData:timestamp callback:^(SLPDataTransferStatus status, id data) {
-        
         BinatoneHistoryData *history= [SLPBLESharedManager analyseOriginalData:data sex:1];
         [weakSelf unshowLoadingView];
     }];
@@ -186,6 +185,7 @@ enum {
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return Section_Bottom;
 }
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSInteger row = 1;
     if (section == Section_RealData) {
